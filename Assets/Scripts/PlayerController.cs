26e034a7b8675315fee0dacfa9d2327578147a6c
Denis Trailin <denis.trailin@gmail.com>
2016-01-23 09:39:56 -0800

    got flight controls actually working decent

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,47 +1,45 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-[System.Serializable]
-public class Boundary
-{
-    public float xMin, xMax, zMin, zMax;
-}
+
 
 public class PlayerController : MonoBehaviour
 {
-    public float tilt;
-    public Boundary boundary;
 
+    public float AmbientSpeed = 100.0f;
 
-    private Rigidbody rb;
+    public float RotationSpeed = 100.0f;
 
+    Rigidbody rb;
+
+    // Use this for initialization
     void Start()
     {
         rb = GetComponent<Rigidbody>();
     }
 
-    public float velocity = 5.0f;
-    public float speed = 10.0f;
-    public float planeLift = 1.965f;
-
-    public float rollRate = 1.0f ;
-    public float elevatorRate =1.0f;
-     public float rudderRate=1.0f;
-
+ 
 
     void FixedUpdate()
     {
+        UpdateFunction();
+    }
 
-  
- 
+    void UpdateFunction()
+    {
 
-
-        rb.AddRelativeForce(Vector3.up * velocity * planeLift);
-
-        rb.AddRelativeTorque(0, 0, -Input.GetAxis("Horizontal") * rollRate);
-
-        rb.AddRelativeForce(0, 0, speed);
-
-        rb.AddRelativeTorque(-Input.GetAxis("Vertical") * elevatorRate, 0, 0);
+        Quaternion AddRot = Quaternion.identity;
+        float roll = 0;
+        float pitch = 0;
+        float yaw = 0;
+        roll = Input.GetAxis("Vertical") * (Time.fixedDeltaTime * RotationSpeed);
+        pitch = Input.GetAxis("Vertical") * (Time.fixedDeltaTime * RotationSpeed);
+        yaw = Input.GetAxis("Horizontal") * (Time.fixedDeltaTime * RotationSpeed);
+        AddRot.eulerAngles = new Vector3(-pitch, yaw, -roll);
+        rb.rotation *= AddRot;
+        Vector3 AddPos = Vector3.forward;
+        AddPos = rb.rotation * AddPos;
+        rb.velocity = AddPos * (Time.fixedDeltaTime * AmbientSpeed);
+        Debug.Log(AddPos * (Time.fixedDeltaTime * AmbientSpeed));
     }
 }
